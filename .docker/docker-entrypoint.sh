a7c6fb95fc7c98d116c1e0d372e92affd988f23e
Rob <rkeplin@gmail.com>
2019-08-21 20:15:30 -0400

    adjusted deployment

--- .docker/docker-entrypoint.sh
+++ .docker/docker-entrypoint.sh
@@ -3,10 +3,12 @@ set -e
 
 if [ ! -z "${URL}" ]; then
     sed -i "s|http://localhost:8082|${URL}|g" /var/www/html/build/env.js
+    sed -i "s|http://bible-ui.rkeplin.local|${URL}|g" /var/www/html/build/env.js
 fi
 
 if [ ! -z "${API_URL}" ]; then
     sed -i "s|http://localhost:8083|${API_URL}|g" /var/www/html/build/env.js
+    sed -i "s|http://bible-go-api.rkeplin.local/v1|${API_URL}|g" /var/www/html/build/env.js
 fi
 
 exec "$@"
